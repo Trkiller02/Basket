74e138b93e589f8aad69a704dd5a6ee94a65818c
Trkiller02 <trollkiller0207@gmail.com>
2025-03-23 21:31:40 -0400

    feat(app):AddPPRSupportNextCanary

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	experimental: {
+		ppr: "incremental",
+		optimizeCss: true,
+		optimizePackageImports: ["@heroui/*"],
+	},
 };
 
 export default nextConfig;
